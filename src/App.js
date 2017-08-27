728b90852b5709da59a8028026faf32e4d37994c
Kendrick Gardner <kbgar24@gmail.com>
2017-08-27 12:59:57 -0500

    Adding temp msg in UI when creating new todo item

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import {TodoForm, TodoList, Footer} from './components/todo';
 import {addTodo, generateID, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todoHelpers';
 import {pipe, partial} from './lib/utils';
-import {loadTodos} from './lib/todoService';
+import {loadTodos, createTodo} from './lib/todoService';
 
 class App extends Component {
   state = {
@@ -39,8 +39,15 @@ class App extends Component {
       currentTodo: '',        
       errorMessage: ''
     })
-   
+    createTodo(newTodo)
+      .then(()=> this.showTempMessage('Todo Added')
+      )   
 
+  }
+
+  showTempMessage = (msg) => {
+    this.setState({message: msg})
+    setTimeout(() => this.setState({message: ''}), 2500)
   }
 
   handleEmptySubmit =(evt) => {
@@ -82,6 +89,7 @@ class App extends Component {
         </div>
         <div className="Todo-App">
           {this.state.errorMessage && <span className="error">{this.state.errorMessage}</span>}
+          {this.state.message && <span className="success">{this.state.message}</span>}
           <TodoForm handleInputChange={this.handleInputChange}
             currentTodo={this.state.currentTodo} 
             handleSubmit={submitHandler}
